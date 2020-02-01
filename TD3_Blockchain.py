a1fe145c8f343053d6829352a4086483ff300688
Thibault <thibault.rieul@gmail.com>
2020-02-01 10:52:23 +0100

    Debut implementation candle

--- TD3_Blockchain.py
+++ TD3_Blockchain.py
@@ -63,6 +63,32 @@ def getDepth(direction='ask', pair = 'BTC-USD'):
     print(text_j[direction+'s'])
 
     
+def refreshDataCandle(pair = 'BTC-USD', duration = '5m'):
+    unite_temps=duration[len(duration)-1]
+    rtime=int(duration[0:len(duration)-1])
+    L_u=[1,60,3600,3600*24,3600*24*7,3600*24*7*4]
+    if unite_temps=="m":
+        rtime=rtime*60
+    elif unite_temps=="h":
+        rtime=rtime*3600
+    elif unite_temps=="d":
+        rtime=rtime*3600*24
+    elif unite_temps=="w":
+        rtime=rtime*3600*24*7
+    elif unite_temps=="M":
+        rtime=rtime*3600*24*7*4
+    elif unite_temps=="s":
+        rtime=rtime
+    else:
+        print("erreur: unité incorrecte veuillez entrer le numero correspondant à l'unité desirée: \n 1-Seconde\n 2-Minute\n 3-Heure\n 4-Jour\n 5-Semaine\n 6-Mois(4 semaines)\n")
+        choix=input()
+        rtime=rtime*L_u[choix-1]  
+    
+    r = requests.get("https://api.pro.coinbase.com//products/"+pair+"/candles?granularity="+str(rtime))
+    #text_j=json.loads(r.text)
+    print(r.text)
+
+refreshDataCandle()  
 
 
 
